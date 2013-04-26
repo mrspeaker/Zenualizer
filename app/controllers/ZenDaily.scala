0e7fdb9c3f38278895135e97a1c3ac68f9903c47
Mr Speaker <mrspeaker@gmail.com>
2013-04-26 12:54:49 +0200

    background

---
+++ app/controllers/ZenDaily.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.iteratee._
+import play.api.libs.concurrent.Promise
+import play.api.libs.ws._
+import play.api.libs.oauth._
+import views._
+import play.api.libs.json.{Json, JsValue}
+
+object ZenDaily extends Controller {
+
+
+}
