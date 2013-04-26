b3e8f87ea4023954b634201259f43ccb6f114778
Mr Speaker <mrspeaker@gmail.com>
2013-04-26 14:27:17 +0200

    twitter printing

--- app/assets/widgets/twitter/main.js
+++ app/assets/widgets/twitter/main.js
@@ -2,14 +2,22 @@
 
 Widget("twitter", {
   init: function (node) {
-  	return this.update(node);
+    return this.update(node);
   },
   rate: 60000,
   update: function (node) {
     return $.getJSON("/timelineAll").then(function (json) {
-    	var head = json[Math.random() * json.length | 0];
-      node.find(".temp").text(head.user.screen_name);
-      node.find(".descr").text(head.text);
+      function render () {
+        var head = json[Math.random() * json.length | 0];
+        node.find(".tweeter").text(head.user.screen_name);
+        node.find(".tweet").text(head.text);
+        setTimeout(function () {
+          render();
+        }, 5000);
+      }
+
+      render();
+
     });
   }
 });
