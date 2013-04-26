6207599f0f6ef59e9312b372f664bae2948ae63c
Mr Speaker <mrspeaker@gmail.com>
2013-04-26 16:24:14 +0200

    add hackernews module

---
+++ app/assets/widgets/hackernews/main.js
@@ -0,0 +1,35 @@
+// Hackernews
+
+Widget("hackernews", {
+  timer: null,
+  init: function (node) {
+    return this.update(node);
+  },
+  rate: 60000,
+  update: function (node) {
+
+    var self = this;
+
+    this.timer && clearTimeout(this.timer);
+
+    return $.getJSON("/hackernews/stream").then(function (json) {
+
+      console.log(json);
+
+      function render () {
+
+        var head = json[Math.random() * json.length | 0];
+        node.find(".user_img").empty().append($("<img>").attr("src", head.user.profile_image_url));
+        node.find(".tweet").text(head.text);
+
+        self.timer = setTimeout(function () {
+          render();
+        }, 5000);
+
+      }
+
+      render();
+
+    });
+  }
+});
