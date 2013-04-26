00340145035a70563470bd38d006f550d81a2011
Pascal Voitot <pascal.voitot.dev@gmail.com>
2013-04-26 11:23:56 +0200

    timeline Json reads

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,7 @@ import scala.concurrent._
 import play.api.libs.ws._
 import play.api.libs.oauth._
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 import play.api.libs.concurrent.Execution.Implicits._
 
@@ -17,14 +18,31 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  val timelineRead = 
+    __.read[Seq[(Long, Boolean, Long, Long)]](
+      Reads.seq(
+        (
+          (__ \ "id").read[Long] and
+          (__ \ "retweeted").read[Boolean] and
+          (__ \ "retweet_count").read[Long] and
+          (__ \ "favorite_count").read[Long]
+        ).tupled
+      )
+    )
+
 
   def timeline = Action { implicit request =>
       Async {
-        retrieveTimeline() map { js => Ok(js) }
+        retrieveTimeline map { js => 
+          timelineRead.reads(js) map { l =>
+            Ok(l.toString)
+          } recoverTotal{ e => BadRequest(JsError.toFlatJson(e)) }
+          
+        }
       }
   }
 
-  def retrieveTimeline(): Future[JsValue] = {
+  def retrieveTimeline: Future[JsValue] = {
     WS.url("https://api.twitter.com/1/statuses/home_timeline.json")
       .sign(OAuthCalculator(
         Twitter.KEY, 
