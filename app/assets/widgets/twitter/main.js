f29c39c51fc04c9352c3633f7cc0f9498f4a18f5
Mr Speaker <mrspeaker@gmail.com>
2013-04-26 15:22:28 +0200

    twitter imgs

--- app/assets/widgets/twitter/main.js
+++ app/assets/widgets/twitter/main.js
@@ -1,19 +1,35 @@
 // Twitter
 
 Widget("twitter", {
+  timer: null,
   init: function (node) {
     return this.update(node);
   },
   rate: 60000,
   update: function (node) {
+
+    var self = this;
+
+    this.timer && clearTimeout(this.timer);
+
     return $.getJSON("/timelineAll").then(function (json) {
       function render () {
+
         var head = json[Math.random() * json.length | 0];
         node.find(".tweeter").text(head.user.screen_name);
         node.find(".tweet").text(head.text);
-        setTimeout(function () {
+        var userImg = node.find(".user_img img"),
+          userImg2 = node.find(".user_img_2 img");
+
+        console.log(userImg.length, userImg2.length);
+        node.find(".user_img_3").empty().append(userImg2);
+        node.find(".user_img_2").empty().append(userImg);
+        node.find(".user_img").empty().append($("<img>").attr("src", head.user.profile_image_url));
+
+        self.timer = setTimeout(function () {
           render();
         }, 5000);
+
       }
 
       render();
